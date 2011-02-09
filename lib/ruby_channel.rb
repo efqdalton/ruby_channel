29002ba19d0673942065f6cbd2b8193573aabc1f
Dalton Pinto <efqdalton@gmail.com>
2011-02-09 11:36:55 -0200

    Removed absolute path on gem loading

--- lib/ruby_channel.rb
+++ lib/ruby_channel.rb
@@ -2,6 +2,6 @@ require 'monitor'
 require 'thread'
 
 load 'ruby_channel/version.rb'
-gem_path = File.dirname __FILE__
-autoload :Channel,  File.join(gem_path, "ruby_channel/channel.rb")
-autoload :Selector, File.join(gem_path, "ruby_channel/selector.rb")
+
+autoload :Channel,  "ruby_channel/channel.rb"
+autoload :Selector, "ruby_channel/selector.rb"
